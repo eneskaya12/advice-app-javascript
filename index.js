bcdf6d65f939fb31959315d4b34d29a00317089f
Enes Kaya <eneskaya5261@gmail.com>
2022-08-09 10:07:19 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const advice = document.querySelector("#advice");
+const title = document.querySelector("#title");
+const titleHeader = document.createElement("h2");
+
+// For showing an advice when refresh the site.
+newAdvice();
+
+// Using fetch API with async/await for getting data from url.
+async function newAdvice(){ 
+
+    let response = await fetch("https://api.adviceslip.com/advice");
+    let data = await response.json();
+    
+    showAdvice(data);
+}
+
+// Changing innerHTML with the data which we took from url.
+function showAdvice(data){
+
+    advice.innerHTML = data.slip.advice;
+    titleHeader.innerHTML = `ADVICE ♦ ${data.slip.id}`;
+    title.appendChild(titleHeader);
+}
+
+// Adding addEventListener to the button for running newAdvice() function.
+const adviceButton = document.querySelector("#btn");  
+adviceButton.addEventListener("click",newAdvice);
